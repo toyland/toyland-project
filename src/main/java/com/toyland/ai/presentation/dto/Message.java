9e52efa84e2096bb572c7855ebdc30f1ed484dda
khloe08 <owlnae04@gmail.com>
2025-02-14 18:36:58 +0900

    [#5] feat: 문답 생성

---
+++ src/main/java/com/toyland/ai/presentation/dto/Message.java
@@ -0,0 +1,14 @@
+package com.toyland.ai.presentation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    private String role;
+    private String content;
+}
