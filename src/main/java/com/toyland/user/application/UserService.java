232235b87e4d37adb42a9ff80726df73f7bae9a9
letsgilit <wjdrlf5781@naver.com>
2025-02-15 17:28:53 +0900

    [#10]feat: 시큐리티 설정 회원 로그인 구현
    - 사용자 인증을 위한 시큐리티 설정 추가
    - 로그인 유효성 검사
    - 비밀번호 암호화 추가

--- src/main/java/com/toyland/user/application/UserService.java
+++ src/main/java/com/toyland/user/application/UserService.java
@@ -3,10 +3,11 @@ package com.toyland.user.application;
 import com.toyland.user.model.User;
 import com.toyland.user.model.UserRoleEnum;
 import com.toyland.user.model.repository.UserRepository;
+import com.toyland.user.presentation.dto.LoginRequestDto;
 import com.toyland.user.presentation.dto.SignupRequestDto;
 import com.toyland.user.presentation.dto.SignupResponseDto;
 import lombok.RequiredArgsConstructor;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,18 +15,38 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     /**
-     * 멤버별 판매 주문 내역 리스트를 조회합니다.
+     * 회원 유저 정보를 저장합니다.
      * @param requestDto 로그인 유저 정보
      * @return 저장된 회원정보
      */
     public SignupResponseDto signup(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
+        String password = passwordEncoder.encode(requestDto.getPassword());
         UserRoleEnum role = requestDto.getRole();
 
         User user = userRepository.save(new User(username, password, role));
         return new SignupResponseDto(user);
     }
+
+    /**
+     * 로그인시 사용자 정보 확인
+     * @param requestDto 로그인 유저 정보
+     * @return 저장된 회원정보
+     */
+    public void login(LoginRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        if(!passwordEncoder.matches(password,user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
+
 }
--- src/main/java/com/toyland/user/presentation/UserController.java
+++ src/main/java/com/toyland/user/presentation/UserController.java
@@ -1,6 +1,7 @@
 package com.toyland.user.presentation;
 
 import com.toyland.user.application.UserService;
+import com.toyland.user.presentation.dto.LoginRequestDto;
 import com.toyland.user.presentation.dto.SignupRequestDto;
 import com.toyland.user.presentation.dto.SignupResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,7 @@ public class UserController {
     private final UserService userService;
 
     /**
-     * 멤버별 판매 주문 내역 리스트를 조회합니다.
+     * 회원가입 요첨 API
      * @param requestDto 로그인 유저 정보
      * @return 회원정보를 포함한 성공적인 응답 생성
      */
@@ -23,6 +24,20 @@ public class UserController {
     public ResponseEntity<SignupResponseDto> signup(@RequestBody SignupRequestDto requestDto) {
         SignupResponseDto responseDto = userService.signup(requestDto);
         return ResponseEntity.ok().body(responseDto);
+    }
+    /**
+     * 로그인 요청 API
+     * @param requestDto 로그인 유저 ID,password
+     * @return 회원정보를 포함한 성공적인 응답 생성
+     */
+    @PostMapping("/users/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto requestDto) {
+        try {
+            userService.login(requestDto);
+            return ResponseEntity.ok().body("로그인 성공");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
