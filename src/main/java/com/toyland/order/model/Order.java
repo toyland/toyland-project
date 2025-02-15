a56abda8b748cde2dcab33fde90e67cce15075f4
jin2304 <search2304@naver.com>
2025-02-15 23:32:15 +0900

    [#15] remove: @Setter 삭제

--- src/main/java/com/toyland/order/model/Order.java
+++ src/main/java/com/toyland/order/model/Order.java
@@ -3,12 +3,10 @@ package com.toyland.order.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.UUID;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "p_order")
