2417a85bd1d64e36a4ff01f13600f712bb7d3ab0
hanjihoon03 <gkswlgns7653@gmail.com>
2025-02-15 14:25:28 +0900

    [#11] refactor: BaseEntity delete 기록 자동화 기능 수정

--- src/main/java/com/toyland/global/config/auditing/JpaAuditingConfig.java
+++ src/main/java/com/toyland/global/config/auditing/JpaAuditingConfig.java
@@ -1,16 +1,11 @@
 package com.toyland.global.config.auditing;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfig {
 
-    @Bean
-    public AuditorAware<Long> auditorAware() {
-        return new AuditorAwareImpl();
-    }
+
 }
