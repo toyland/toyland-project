12964acfe692291055cb0e14af960a04d0e3a60e
letsgilit <wjdrlf5781@naver.com>
2025-02-15 17:28:53 +0900

    [#10]feat: 시큐리티 설정 회원 로그인 구현
    - 사용자 인증을 위한 시큐리티 설정 추가
    - 로그인 유효성 검사
    - 비밀번호 암호화 추가

---
+++ src/main/java/com/toyland/global/config/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.toyland.global.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable());
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/api/v1/**").permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
