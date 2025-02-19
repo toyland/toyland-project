4b76ef03d3ffd2ee354187701ac2a9e99df4f3be
je-pa <jieun0502@gmail.com>
2025-02-19 22:03:40 +0900

    [#87] feat: category rud api 구현

---
+++ src/main/java/com/toyland/category/application/usecase/dto/UpdateCategoryServiceRequestDto.java
@@ -0,0 +1,29 @@
+package com.toyland.category.application.usecase.dto;
+
+import com.toyland.category.presentation.UpdateCategoryRequestDto;
+import java.util.List;
+import java.util.UUID;
+import lombok.Builder;
+
+@Builder
+public record UpdateCategoryServiceRequestDto (
+    UUID categoryId, String name, UUID parentCategoryId
+) {
+
+  public static UpdateCategoryServiceRequestDto of(
+      UpdateCategoryRequestDto request, UUID categoryId) {
+    return UpdateCategoryServiceRequestDto.builder()
+        .name(request.name())
+        .categoryId(categoryId)
+        .parentCategoryId(request.parentCategoryId())
+        .build();
+  }
+
+  public boolean isEqualsParentCategoryId(){
+    return this.categoryId.equals(this.parentCategoryId);
+  }
+
+  public List<UUID> getCategoryList(){
+    return parentCategoryId == null ? List.of(categoryId) : List.of(categoryId, parentCategoryId);
+  }
+}
