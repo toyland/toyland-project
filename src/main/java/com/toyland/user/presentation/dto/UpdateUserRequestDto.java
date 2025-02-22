a50a383c82c888ac1da24c424add250dbc05d483
letsgilit <wjdrlf5781@naver.com>
2025-02-22 18:45:36 +0900

    #138 refactor: Swagger 적용 -swagger annotation 추가 -Service transaction 추가 -pageSize 조건 추가

--- src/main/java/com/toyland/user/presentation/dto/UpdateUserRequestDto.java
+++ src/main/java/com/toyland/user/presentation/dto/UpdateUserRequestDto.java
@@ -1,6 +1,5 @@
 package com.toyland.user.presentation.dto;
 
-import com.toyland.user.model.UserRoleEnum;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -11,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdateUserRequestDto {
 
+    private Long userId;
+
     @Size(min = 4, max = 10, message = "사용자 이름은 4자 이상 10자 이하이어야 합니다.")
     @Pattern(regexp = "^[a-z0-9]+$", message = "사용자 이름은 소문자 알파벳과 숫자로만 구성되어야 합니다.")
     private String username;
@@ -20,13 +21,10 @@ public class UpdateUserRequestDto {
             message = "비밀번호는 최소 8자 이상 15자 이하이며, 대소문자, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
 
-    private UserRoleEnum role;
-
     @Builder
-    public UpdateUserRequestDto(String username, String password, UserRoleEnum role) {
+    public UpdateUserRequestDto(String username, String password) {
         this.username = username;
         this.password = password;
-        this.role = role;
     }
 
     public void encodePassword(String password) {
